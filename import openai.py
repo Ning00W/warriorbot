0ab848a9fe702afd0827e66fd632b437bf6ec92c
Ning00W <117543707+Ning00W@users.noreply.github.com>
2023-04-21 19:00:10 +0100

    Add files via upload

---
+++ import openai.py
@@ -0,0 +1,21 @@
+import openai
+
+with open("keys.txt") as f:
+	# converting our text file to a list of lines
+	lines = f.read().split('\n')
+	#openai.api_key = lines[0]
+	# discord token
+	DISCORD_TOKEN = lines[1]
+# close the file
+f.close()
+
+response = openai.Image.create(
+	prompt="a beautiful London spring day, vintage vibes, muted colors",
+	n=1
+	size+"1024X1024"
+	)
+	image_url = response['data'][0]['url']
+	print(image_url)
+	await message.channel.send("image_url")
+
+client.run(DISCORD_TOKEN)
